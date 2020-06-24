e8be02af9a7bfc8dfd10aade7be8790db7e3b5f2
driouechemed <drioueche.med@gmail.com>
2020-06-24 23:36:59 +0100

    login completed

--- frontend/login/src/components/loginFormComponent.js
+++ frontend/login/src/components/loginFormComponent.js
@@ -2,36 +2,82 @@ import React, { Component }  from 'react';
 import { StyleSheet, View, Text, TextInput, Button, Alert } from 'react-native';
 import ValidationComponent from 'react-native-form-validator';
 import { globalStyles } from '../styles/globalStyles';
+import { API } from '../shared/api';
 
 export default class LoginForm extends ValidationComponent {
 
   constructor(props) {
     super(props);
     this.state = {
-      email: '',
+      username: '',
       password:'',
     };
   }
 
   _onPressButton() {
     this.validate({
-      email: {email: true, required: true},
+      username: {minlength:2, required: true},
       password: {minlength: 3, maxlength: 24, required: true},
     });
     if(this.isFormValid()){
       //console.log('Form Valid ...');
-      this.setState({
-        email: '',
-        password:'',
-      });
-      Alert.alert(
-        "Congra!",
-        "Login Form Valid",
-        [
-          { text: "OK", onPress: () => console.log("OK Pressed") }
-        ],
-        { cancelable: false }
-      );
+      var api = API.concat('/token/obtain/');
+      fetch(api, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: this.state.email,
+          username: this.state.username,
+          password: this.state.password
+        }),
+      })
+      .then(res => {
+        console.log("res.status: ", res.status);
+        switch (res.status) {
+          case 200:
+            console.log('success');
+            this.setState({
+              username: '',
+              password:'',
+            });
+            Alert.alert(
+              "Congra!",
+              "Connected",
+              [
+                { text: "OK", onPress: () => console.log("OK Pressed") }
+              ],
+              { cancelable: false }
+            );
+            break;
+          case 401:
+            Alert.alert(
+              "OPS!",
+              "Username or Password Incorrect",
+              [
+                { text: "OK", onPress: () => console.log("OK Pressed") }
+              ],
+              { cancelable: false }
+            );
+            console.log('can\'t obtain token');
+            break;
+          case 500:
+            console.log('server error, try again');
+            break;
+          default:
+            console.log('unhandled');
+            break;
+        }
+      })
+      .catch(err => {
+        console.error(err)
+      })
+
+      //////////////
+
+
     }
   }
 
@@ -40,10 +86,10 @@ export default class LoginForm extends ValidationComponent {
         <View>
           <TextInput
             style={globalStyles.input}
-            placeholder='Email'
-            value={this.state.email}
-            onChangeText={(email) => this.setState({email})}
-            onBlur={() => this.validate({email: {email: true, required: true}})}
+            placeholder='Username'
+            value={this.state.username}
+            onChangeText={(username) => this.setState({username})}
+            onBlur={() => this.validate({username: {minlength:2, required: true}})}
           />
           <TextInput
             secureTextEntry={true}
